361bff80c7a8b09f90f7dc5831e422f7109be1ce
asdhgfasd <asdhgfasd@naver.com>
2017-02-16 09:42:09 +0900

    디렉토리 변경

---
+++ src/test/java/com/gncloud/fastcat/module/hanEngConverter/AlphaToHanTest.java
@@ -0,0 +1,29 @@
+package com.gncloud.fastcat.module.hanEngConverter;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class AlphaToHanTest {
+
+	@Test
+	public void testAlphaToHan() {
+		AlphaToHan ath = new AlphaToHan();
+		HanToAlpha hta = new HanToAlpha();
+
+		// 알파벳 한글 변환 테스트
+		assertEquals("뷁뷁뷁뷁", ath.alphaToHan("qnpfrqnpfrqnpfrqnpfr"));
+		assertEquals("무궁화꽃이피었습니다",ath.alphaToHan("anrndghkRhcdlvldjTtmqslek"));
+		assertEquals("가나다라마바사", ath.alphaToHan("rkskekfkakqktk"));
+		assertEquals("안녕 하세요", ath.alphaToHan("dkssud gktpdy"));
+		assertEquals("까르뽀나라", ath.alphaToHan("RkfmQhskfk"));
+
+		//한글 알파벳 변환 테스트
+		assertEquals("gksrmf",hta.hanToAlpha("한글"));
+		assertEquals("gks rmf",hta.hanToAlpha("한 글"));
+		assertEquals("rsekfk",hta.hanToAlpha("ㄱㄴ다라"));
+		assertEquals("rs ekfk",hta.hanToAlpha("ㄱㄴ 다라"));
+		assertEquals("rsasd/fekfk",hta.hanToAlpha("ㄱㄴasd/f다라"));
+		assertEquals("rfxefgkgkssef",hta.hanToAlpha("ㄱㄾㄷㄹ하하ㄴㄴㄷㄹ"));
+	}
+}
