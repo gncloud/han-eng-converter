88e29dc14ba34056f637afd6fde961a02226dde4
안성민 <asdhgfasd@naver.com>
2017-02-17 17:17:34 +0900

    최적 키워드 리턴 클래스 Coverter 생성 커밋

---
+++ src/main/java/com/gncloud/fastcat/module/hanEngConverter/Converter.java
@@ -0,0 +1,11 @@
+package com.gncloud.fastcat.module.hanEngConverter;
+
+/**
+ * Created by gncloud on 2017-02-17.
+ */
+public class Converter { //입력된 랜덤 스트링을 최적의 검색결과로 전달 해주는 클래스
+    public static void main(String[] args){
+
+    }
+    
+}
