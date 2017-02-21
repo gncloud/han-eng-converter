718ca39d4e71180e36e7706f697df6e9204c58d1
안성민 <asdhgfasd@naver.com>
2017-02-21 15:32:14 +0900

    사전파일 키워드 insert 테스트

--- src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicCustomInsert.java
+++ src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicCustomInsert.java
@@ -1,11 +1,44 @@
 package com.gncloud.fastcat.module.hanEngConverter;
 
 
+import net.sf.extjwnl.JWNLException;
+import net.sf.extjwnl.JWNLIOException;
+import net.sf.extjwnl.data.*;
+import net.sf.extjwnl.dictionary.Dictionary;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Iterator;
+
 /**
  * Created by gncloud on 2017-02-21.
  */
 public class DicCustomInsert {
-    public void insertKeyword(String keyword){
+    public void insertKeyword(String keyword) {
+        try{
+            FileInputStream inputStream = new FileInputStream("./dic/file_properties.xml");
+            Dictionary dictionary = Dictionary.getInstance(inputStream);
+            Iterator<Synset> synset = dictionary.getSynsetIterator(POS.NOUN);
 
+            dictionary.edit();
+            Synset newSynset = new Synset(dictionary, POS.NOUN);
+            IndexWord idxWord = new IndexWord(dictionary, keyword, POS.NOUN,newSynset);
+            Synset topmostSynset = synset.next();
+            Pointer newPointer = new Pointer(PointerType.HYPONYM, topmostSynset, newSynset);
+            topmostSynset.getPointers().add(newPointer);
+            dictionary.save();
+
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (JWNLIOException e){
+            e.printStackTrace();
+        } catch (JWNLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        DicCustomInsert dci = new DicCustomInsert();
+        dci.insertKeyword("chanel");
     }
 }
--- src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicSearch.java
+++ src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicSearch.java
@@ -1,69 +1,47 @@
 package com.gncloud.fastcat.module.hanEngConverter;
 
 
-import net.didion.jwnl.JWNL;
-import net.didion.jwnl.JWNLException;
-import net.didion.jwnl.data.POS;
-import net.didion.jwnl.dictionary.Dictionary;
-import net.didion.jwnl.dictionary.file.DictionaryFile;
+import net.sf.extjwnl.JWNLException;
+import net.sf.extjwnl.JWNLIOException;
+import net.sf.extjwnl.data.*;
+import net.sf.extjwnl.dictionary.Dictionary;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.Iterator;
 
 /**
  * Created by gncloud on 2017-02-16.
  */
 
 public class DicSearch {
-
-    public void search(String keyword){
-        try {
-            long start = System.currentTimeMillis();
-            JWNL.initialize(new FileInputStream("./dic/file_properties.xml"));
-            Dictionary dic = Dictionary.getInstance();
-            String resNone = dic.getIndexWord(POS.NOUN, keyword) != null ? dic.getIndexWord(POS.NOUN, keyword).toString():"";
-            String resAdjective = dic.getIndexWord(POS.ADJECTIVE, keyword) !=null? dic.getIndexWord(POS.ADJECTIVE, keyword).toString():"";
-            String resAdverb = dic.getIndexWord(POS.ADVERB, keyword) !=null? dic.getIndexWord(POS.ADVERB, keyword).toString():"";
-            String resVerb = dic.getIndexWord(POS.VERB,keyword) != null? dic.getIndexWord(POS.VERB, keyword).toString():"";
-
-            long end = System.currentTimeMillis();
-            System.out.println( "실행 시간 : " + ( end - start )/1000.0 );
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (JWNLException e){
-            e.printStackTrace();
-        }
-    }
-
     public int searchResultCount(String keyword){
         // 0 -> 검색결과 없음
         // -1 -> 사전검색 Exception
         // 1 -> 검색결과 있음
 
-        try {
-            JWNL.initialize(new FileInputStream("./dic/file_properties.xml"));
-            Dictionary dic = Dictionary.getInstance();
-            String resNone = dic.getIndexWord(POS.NOUN, keyword) != null ? dic.getIndexWord(POS.NOUN, keyword).toString():"";
-            String resAdjective = dic.getIndexWord(POS.ADJECTIVE, keyword) !=null? dic.getIndexWord(POS.ADJECTIVE, keyword).toString():"";
-            String resAdverb = dic.getIndexWord(POS.ADVERB, keyword) !=null? dic.getIndexWord(POS.ADVERB, keyword).toString():"";
-            String resVerb = dic.getIndexWord(POS.VERB,keyword) != null? dic.getIndexWord(POS.VERB, keyword).toString():"";
+        try{
+            FileInputStream inputStream = new FileInputStream("./dic/file_properties.xml");
+            Dictionary dictionary = Dictionary.getInstance(inputStream);
 
-            if(resNone.length() + resAdjective.length() + resAdverb.length() + resVerb.length() <= 0){
-                return 0;
-            }
-            return 1;
-        } catch (FileNotFoundException e) {
+            System.out.println(dictionary.getIndexWord(POS.NOUN, keyword));
+
+        }catch (FileNotFoundException e){
             e.printStackTrace();
-            return -1;
-        } catch (JWNLException e){
+        }catch (JWNLIOException e){
             e.printStackTrace();
-            return -1;
+        } catch (JWNLException e) {
+            e.printStackTrace();
         }
+
+        return 0;
     }
 
     public static void main(String[] args){
         DicSearch ds = new DicSearch();
-        System.out.println(ds.searchResultCount("nike"));
+//        System.out.println(ds.searchResultCount("nike"));
         System.out.println(ds.searchResultCount("adidas"));
+//        System.out.println(ds.searchResultCount("chanel"));
+//        System.out.println(ds.searchResultCount("channel"));
     }
 }
