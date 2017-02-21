a05ea74b5bc882711e70a1ba56dca2498b8771e9
안성민 <asdhgfasd@naver.com>
2017-02-21 18:02:58 +0900

    사전 검색, 커스텀 검색 결과 리턴 메소드 적용

--- src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicCustomInsert.java
+++ src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicCustomInsert.java
@@ -16,41 +16,13 @@ import java.io.*;
  * Created by gncloud on 2017-02-21.
  */
 public class DicCustomInsert {
-//    public void insertKeyword(String keyword) {
-//        try{
-//            FileInputStream inputStream = new FileInputStream("./dic/file_properties.xml");
-//            Dictionary dictionary = Dictionary.getInstance(inputStream);
-//            Iterator<Synset> synset = dictionary.getSynsetIterator(POS.NOUN);
-//
-//            dictionary.edit();
-//            Synset newSynset = new Synset(dictionary, POS.NOUN);
-//            IndexWord idxWord = new IndexWord(dictionary, keyword, POS.NOUN,newSynset);
-//            Synset topmostSynset = synset.next();
-//            Pointer newPointer = new Pointer(PointerType.HYPONYM, topmostSynset, newSynset);
-//            topmostSynset.getPointers().add(newPointer);
-//
-//            dictionary.save();
-//            dictionary.close();
-//            inputStream.close();
-//
-//        }catch (FileNotFoundException e){
-//            e.printStackTrace();
-//        }catch (JWNLIOException e){
-//            e.printStackTrace();
-//        } catch (JWNLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     public void insertKeyword(String keyword){
         int match = 0;
+        String filepath = "./dic/dict/custom.txt";
         BufferedReader in = null;
-        BufferedWriter bw = null;
         FileWriter fw = null;
         try {
-            in = new BufferedReader(new FileReader("./dic/dict/custom.txt"));
+            in = new BufferedReader(new FileReader(filepath));
             String s = "";
             while((s = in.readLine())!=null){
                 if(s.equalsIgnoreCase(keyword)){
@@ -60,7 +32,7 @@ public class DicCustomInsert {
             }
             in.close();
             if(match != 1) {
-                fw = new FileWriter("./dic/dict/custom.txt", true);
+                fw = new FileWriter(filepath, true);
                 fw.write("\n"+keyword);
             }
         } catch (FileNotFoundException e) {
@@ -82,6 +54,6 @@ public class DicCustomInsert {
 
     public static void main(String[] args){
         DicCustomInsert dci = new DicCustomInsert();
-        dci.insertKeyword("adidas");
+        dci.insertKeyword("asdf");
     }
 }
--- src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicSearch.java
+++ src/main/java/com/gncloud/fastcat/module/hanEngConverter/DicSearch.java
@@ -6,9 +6,7 @@ import net.sf.extjwnl.JWNLIOException;
 import net.sf.extjwnl.data.*;
 import net.sf.extjwnl.dictionary.Dictionary;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 
 /**
@@ -20,29 +18,74 @@ public class DicSearch {
         // 0 -> 검색결과 없음
         // -1 -> 사전검색 Exception
         // 1 -> 검색결과 있음
+        FileInputStream dicInputStream = null;
+        String filepath = "./dic/dict/custom.txt";
+        BufferedReader in = null;
+        Dictionary dictionary = null;
+        int match = 0;
         try{
-            FileInputStream dicInputStream = new FileInputStream("./dic/file_properties.xml");
-            Dictionary dictionary = Dictionary.getInstance(dicInputStream);
-            System.out.println(dictionary.getIndexWord(POS.NOUN, keyword));
+            dicInputStream = new FileInputStream("./dic/file_properties.xml");
+            dictionary = Dictionary.getInstance(dicInputStream);
+            String resNone = dictionary.getIndexWord(POS.NOUN, keyword) != null ? dictionary.getIndexWord(POS.NOUN, keyword).toString() :"";
+            String resAdjective = dictionary.getIndexWord(POS.ADJECTIVE, keyword) != null ? dictionary.getIndexWord(POS.ADJECTIVE, keyword).toString() :"";
+            String resAdverb = dictionary.getIndexWord(POS.ADVERB, keyword) != null ? dictionary.getIndexWord(POS.ADVERB, keyword).toString() :"";
+            String resVerb = dictionary.getIndexWord(POS.VERB, keyword) != null ? dictionary.getIndexWord(POS.VERB, keyword).toString() :"";
+            in = new BufferedReader(new FileReader(filepath));
+            String s = "";
+            while((s = in.readLine())!=null){
+                if(s.equalsIgnoreCase(keyword)){
+                    match += 1;
+                    break;
+                }
+            }
+            if(!resNone.isEmpty()){
+                match = 1;
+            }else if(!resAdverb.isEmpty()){
+                match = 1;
+            }else if(!resAdjective.isEmpty()){
+                match = 1;
+            }else if(!resVerb.isEmpty()){
+                match = 1;
+            }else {
+                match += 0;
+            }
+
             dictionary.close();
             dicInputStream.close();
-        }catch (FileNotFoundException e){
+        } catch (FileNotFoundException e){
             e.printStackTrace();
-        }catch (JWNLIOException e){
+            return -1;
+        } catch (JWNLIOException e){
             e.printStackTrace();
+            return -1;
         } catch (JWNLException e) {
             e.printStackTrace();
+            return -1;
         } catch (IOException e) {
             e.printStackTrace();
+            return -1;
+        }finally {
+            if(in != null && dicInputStream !=null && dictionary != null){
+                try {
+                    dictionary.close();
+                    in.close();
+                    dicInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return -1;
+                } catch (JWNLException e) {
+                    e.printStackTrace();
+                    return -1;
+                }
+            }
         }
-
-        return 0;
+        return match;
     }
 
     public static void main(String[] args){
         DicSearch ds = new DicSearch();
 //        System.out.println(ds.searchResultCount("nike"));
-        System.out.println(ds.searchResultCount("adidas"));
+        System.out.println(ds.searchResultCount("asdf"));
 //        System.out.println(ds.searchResultCount("chanel"));
 //        System.out.println(ds.searchResultCount("channel"));
     }
